948537518446cedbb095c54d1bf207530fbd4f6b
WeslleyIvis <weslley_reive@hotmail.com>
2023-03-01 20:57:27 -0300

    slide Selected and hide

--- dist/Slide.js
+++ dist/Slide.js
@@ -1,30 +1,27 @@
 export default class Slide {
-    container: Element;
-    slides: Element[];
-    controls: Element;
-    timer: number;
-    index: number;
-    slide: Element;
-    constructor(container: Element, slides: Element[], controls: Element, timer: number = 5000) {
+    container;
+    slides;
+    controls;
+    timer;
+    index;
+    slide;
+    constructor(container, slides, controls, timer = 5000) {
         this.container = container;
         this.slides = slides;
         this.controls = controls;
         this.timer = timer;
-        
         this.index = 0;
         this.slide = this.slides[this.index];
-
         this.show(2);
     }
-
-    hide(el: Element) {
+    hide(el) {
         el.classList.remove('active');
     }
-
-    show(index: number) {
+    show(index) {
         this.index = index;
-        this.slide = this.slides[this.index]
+        this.slide = this.slides[this.index];
         this.slides.forEach(element => this.hide(element));
         this.slide.classList.add('active');
     }
 }
+//# sourceMappingURL=Slide.js.map
